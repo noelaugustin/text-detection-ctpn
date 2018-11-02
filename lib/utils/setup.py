2c3b6c7787e20b705ff9b769118c2c64625efae3
Noel Augustin <noelaugustin@users.noreply.github.com>
2018-11-02 12:54:06 +0530

    Update setup.py

--- lib/utils/setup.py
+++ lib/utils/setup.py
@@ -3,9 +3,9 @@ import numpy as np
 from distutils.core import setup
 
 try:
-numpy_include = np.get_include()
+  numpy_include = np.get_include()
 except AttributeError:
-numpy_include = np.get_numpy_include()
+  numpy_include = np.get_numpy_include()
 
 setup(
 ext_modules=cythonize(["bbox.pyx","cython_nms.pyx"],include_dirs=[numpy_include]),
